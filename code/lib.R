51fe1c7d2db8476a83573b3194d573df3972aa20
Thiago Montenegro <47747825+thimontenegro@users.noreply.github.com>
2019-07-02 13:51:24 -0300

    Add files via upload

--- code/lib.R
+++ code/lib.R
@@ -1,29 +1,19 @@
-theme_report <- function(base_size = 11,
-                         strip_text_size = 12,
-                         strip_text_margin = 5,
-                         subtitle_size = 13,
-                         subtitle_margin = 10,
-                         plot_title_size = 16,
-                         plot_title_margin = 10,
-                         ...) {
-  ret <- ggplot2::theme_minimal(base_family = "Roboto-Regular",
-                                base_size = base_size, ...)
-  ret$strip.text <- ggplot2::element_text(hjust = 0, size=strip_text_size,
-                                          margin=margin(b=strip_text_margin),
-                                          family="Roboto-Bold")
-  ret$plot.subtitle <- ggplot2::element_text(hjust = 0, size=subtitle_size,
-                                             margin=margin(b=subtitle_margin),
-                                             family="PT Sans")
-  ret$plot.title <- ggplot2::element_text(hjust = 0, size = plot_title_size,
-                                          margin=margin(b=plot_title_margin),
-                                          family="Oswald")
-  ret
-}
-
-import_data <- function(){
-  
-}
-
-read_projectdata <- function(){
-  
+read_projectdata <- function() {
+  readr::read_csv(
+    here::here("data/2017.csv"),
+    col_types = cols(
+      Country = col_character(),
+      Happiness.Rank = col_double(),
+      Happiness.Score = col_double(),
+      Whisker.high = col_double(),
+      Whisker.low = col_double(),
+      Economy..GDP.per.Capita. = col_double(),
+      Family = col_double(),
+      Health..Life.Expectancy. = col_double(),
+      Freedom = col_double(),
+      Generosity = col_double(),
+      Trust..Government.Corruption. = col_double(),
+      Dystopia.Residual = col_double()
+    )
+  )
 }
